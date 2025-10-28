38ac5d4dc3ac39e8acde287cdb9e5ebccfb9d807
Gaxoleana <pralit750@gmail.com>
2025-10-28 10:29:57 +0700

    10.29VT

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+
     void Start()
     {
         base.Initialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            banana.InitWeapon(20, this);
+
+            WaitTime = 0.0f;
+        }
     }
 
     public void OnHitWith(Enemy enemy)
@@ -21,8 +40,15 @@ public class Player : Character
         }
     }
 
+    void FixedUpdate()
+    {
+        //Debug.Log("Update" + Time.fixedDeltaTime);
+        WaitTime = Time.fixedDeltaTime;
+    }
+
     void Update()
     {
-
+        //Debug.Log("Fixed Update" + Time.deltaTime);
+        Shoot();
     }
 }
